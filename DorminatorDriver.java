4f171d13106393047d3402648bad99a88c09f036
Ryan Almizyed <64053382+ryanbrahim@users.noreply.github.com>
2020-09-26 12:19:42 -0500

    Update DorminatorDriver.java

--- DorminatorDriver.java
+++ DorminatorDriver.java
@@ -1,45 +1,72 @@
 import java.util.Scanner;
 
 /**
- * This is the driver for Dorminator program.  It provides the UI for user I/O and calls 
- * the other classes.
+ * This is the driver for Dorminator program. It provides the UI for user I/O and calls the other
+ * classes.
  * 
  * @author Ryan Almizyed
  */
 public class DorminatorDriver
 {
+  
+  private static final String[] userCommands = {"exit", "help", "dorms"};
 
   public static void main(String[] args)
   {
-    //Initialization:
+    // Initialization:
     System.out.println("Hello, and welcome to The Dorminator 99th generation.");
-    System.out.println("Input a dorm name to learn more about that dorm, or type 'exit' to exit.");
-    //Populate dorm database from data file
+    System.out.println("Input a dorm name to learn more about that dorm, 'help' for commands, " 
+    + "or type 'exit' to exit.");
+    // Populate dorm database from data file
     String fileName = "";
-    //HashTableMap<String, String> database = new HashTableMap();
-    //database.populate(fileName);
+    UI<String, String> database = new UI(fileName);
+    database.populate(fileName);
+    System.out.println("Possible dorm names are:");
+    database.getAllKeys();
+
     Scanner scanner = new Scanner(System.in);
-    
-    //Main driver loop
-    boolean exit = false;
-    while(exit != true)
+
+    // Main driver loop
+    while (true)
     {
-      
-      String userInput = scanner.nextLine();
-      //exit condition
-      if(userInput.toLowerCase().equals("exit"))
+      // get user's command
+      String userInput = scanner.nextLine().toLowerCase().trim();
+
+      // exit command
+      if(userInput.equals("exit"))
         break;
       
-      //getValue(String key)   -gets the value for a corresponding key
-      //boolean containsKey()   -checks if database contains the key
-      //getAllKeys()    -print a list of all the keys in database
-      //populate(File fileName) -populates the database with values from a file
-      //
+      //help command
+      if(userInput.equals("help"))
+        printUserCommands();
       
+      //valid dorm name
+      if(database.containsKey(userInput))
+      {
+        
+      }
       
+      //invalid dorm name
+
+      // getValue(String key) -gets the value for a corresponding key
+      // boolean containsKey() -checks if database contains the key
+      // getAllKeys() -print a list of all the keys in database
     }
-    
+
+    // closing program
+    scanner.close();
+    System.out.println("Thank you for using The Dorminator, have a nice day!");
+
 
   }
 
+  /**
+   * Method to print out all of the possible commands the user can call
+   */
+  private static void printUserCommands()
+  {
+    for(String command : userCommands)
+      System.out.print("'" + command + "' ");
+    System.out.println();
+  }
 }
