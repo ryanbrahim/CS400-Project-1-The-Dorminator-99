ff5a8d6d287e9abd25cfa15c2168d10d52159397
neilbhutada <50277952+neilbhutada@users.noreply.github.com>
2020-09-26 22:45:36 +0530

    Create UI.java

---
+++ Neil/UI.java
@@ -0,0 +1,31 @@
+public class UI <KeyType, ValueType>
+{
+	String temp[] = new String[2];
+	HashTableMap htm = new HashTableMap();
+	public UI(String filename) throws FileNotFoundException
+	{
+		 FileInput fi = new FileInput(filename); //
+			while(fi.hasNext())
+			{
+				temp = fi.getNext(); //going to the next line
+				
+			htm.put(temp[0], temp[1]);
+			
+			}
+		
+	}
+	public void getAllKeys()
+	{  
+		htm.getAllKeys();
+	}
+public ValueType getValue(KeyType key)
+{
+ 
+ ValueType returned = (ValueType) htm.get(key);
+ return (ValueType) returned;
+	
+
+}
+
+
+}
