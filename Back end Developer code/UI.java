ade9395208d5e266e89a9c7365c197218fc50ab9
bluepra <71447892+bluepra@users.noreply.github.com>
2020-09-28 23:41:17 -0500

    change getAllKeys UI.java
    changed the way getAllKeys works

--- Back end Developer code/UI.java
+++ Back end Developer code/UI.java
@@ -1,36 +1,62 @@
-  
+// --== CS400 File Header Information ==--
+// Name: Neil Bhuatada
+// Email: ??
+// Team: MG
+// Role: Front-end developer
+// TA: Harit Vishwarkama
+// Lecturer: Florian Heimerl
+// Notes to Grader: <optional extra notes> 
+
+//Complete P1
+
+import java.io.FileNotFoundException;
+
 public class UI <KeyType, ValueType>
 {
 	String temp[] = new String[2];
 	HashTableMap htm = new HashTableMap();
+	String filename;
+	
 	public UI(String filename) throws FileNotFoundException
 	{
-		 FileInput fi = new FileInput(filename); //
+	    this.filename = filename;
+		  FileInput fi = new FileInput(this.filename);
 			while(fi.hasNext())
 			{
 				temp = fi.getNext(); //going to the next line
-				
-			htm.put(temp[0], temp[1]);
-			
+				htm.put(temp[0], temp[1]);
 			}
-		
 	}
-	public void getAllKeys()
-	{  
-		htm.getAllKeys();
-	}
-public ValueType getValue(KeyType key)
-{
- 
- ValueType returned = (ValueType) htm.get(key);
- return (ValueType) returned;
 	
-
-}
-public boolean containsKey(KeyType key) 
-{
-    boolean check = htm.containsKey(key);
-    return check; 
-}
+	public void getAllKeys()
+	{
+    try 
+    {
+      FileInput fi = new FileInput(this.filename);
+      while(fi.hasNext())
+      {
+        temp = fi.getNext(); //going to the next line
+        
+        System.out.println(temp[0]);
+      }
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println(this.filename + " not found");
+      e.printStackTrace();
+    }
+	}
+	
+	public ValueType getValue(KeyType key)
+  {
+	  ValueType returned = (ValueType)htm.get(key);
+	  return (ValueType)returned;
+  }
+	
+  public boolean containsKey(KeyType key) 
+  {
+      boolean check = htm.containsKey(key);
+      return check; 
+  }
 
 }
