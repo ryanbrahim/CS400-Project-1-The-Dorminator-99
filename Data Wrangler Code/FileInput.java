755d8e2aef78a0e459bfe526f54a9ca16738c3b6
bluepra <71447892+bluepra@users.noreply.github.com>
2020-09-29 22:33:26 -0500

    Add javadoc FileInput.java

--- Data Wrangler Code/FileInput.java
+++ Data Wrangler Code/FileInput.java
@@ -1,50 +1,66 @@
+// --== CS400 File Header Information ==--
+// Name: Prannav Arora
+// Email: parora9@wisc.edu
+// Team: MG
+// Role: Data Wrangler
+// TA: Harit Vishwarkama
+// Lecturer: Gary Dahl
+// Notes to Grader: FileInput.java made by Prannav Arora (Data Wrangler)
+
+
 import java.io.File;
-import java.io.FileNotFoundException;  
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.Arrays;
 
-public class FileInput 
-{
-    File f;
-    Scanner sc;
-    String delim = ":";
-    
-    public FileInput(String filename) throws FileNotFoundException
-    {
-      f = new File(filename);
-      if(!f.exists())
-      {
-        throw new FileNotFoundException();
-      }
-      sc = new Scanner(f);
-    }
-    
-    public boolean hasNext()
-    {
-      boolean b =  sc.hasNextLine();
-      return b;
-    }
-    
-    /**
-     * Used to get lines from a text file 
-     * @return the string array of the line with the key at index 0
-     *         and the value at index 1
-     */
-    public String[] getNext()
-    {
-      String line =  sc.nextLine();   //Get line from file
-      line = line.trim();
-      
-      String[] arr = line.split(delim); //Make String array, split by ":"
-      arr[0] = arr[0].trim();
-      arr[1] = arr[1].trim();
-      //System.out.println(Arrays.toString(arr));
-      
-      return arr;
-    }
-    
-  
+/**
+ * This class can be used by backend developers to get data conveniently
+ * 
+ * @author Prannav Arora (parora9@wisc.edu)
+ *
+ */
+public class FileInput {
+  File f; // File we want data from
+  Scanner sc; // Scanner used to get data from file
+  String delim = ":"; // The token that splits a line into a key and a value
 
-  
+  /**
+   * The constructor
+   * 
+   * @param filename - path of the file
+   * @throws FileNotFoundException if path is invalid, or no file is found
+   */
+  public FileInput(String filename) throws FileNotFoundException {
+    f = new File(filename);
+    if (!f.exists()) {
+      throw new FileNotFoundException();
+    }
+    sc = new Scanner(f);
+  }
+
+  /**
+   * Checks to see if the scanner has more information
+   * 
+   * @return true if more lines in file to obtain, false otherwise
+   */
+  public boolean hasNext() {
+    boolean b = sc.hasNextLine();
+    return b;
+  }
+
+  /**
+   * Gets a line from the text file, in a nice key/value pair format
+   * 
+   * @return the string array of the line: [key, value]
+   */
+  public String[] getNext() {
+    String line = sc.nextLine(); // Get line from file
+    line = line.trim();
+
+    String[] arr = line.split(delim); // Make String array, split by ":"
+    arr[0] = arr[0].trim();
+    arr[1] = arr[1].trim();
+
+    return arr;
+  }
 
 }
